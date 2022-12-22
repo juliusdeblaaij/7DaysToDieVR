5c917c8e59f658d6a735c2ca2ab86c542a638770
Julius de Blaaij <julius.de.blaaij@gmail.com>
2022-12-22 22:31:21 +0100

    Initial commit
    Added VorpX wrapper class, API class which currently updates the player camera based on VR headset rotation, and performs absolute movement

--- API.cs
+++ API.cs
@@ -4,42 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static XUiC_ArchetypesWindowGroup;
-using ServerTools;
+using HarmonyLib;
+using UnityEngine;
+using System.Reflection;
+using InControl;
 
 namespace _7DaysToDieVR
 {
     public class API : IModApi
     {
-        private string ServerChatName = "Server";
+        public static float VRHeadsetFOV = 90f;
 
-        public void InitMod(Mod _modInstance)
+        public void InitMod(Mod mod)
         {
-            //This registers a handler for when the server handles a chat message.
-            ModEvents.ChatMessage.RegisterHandler(ChatMessage);
+            Log.Out(" Loading Patch: " + GetType().ToString());
+            new Harmony(GetType().ToString()).PatchAll(Assembly.GetExecutingAssembly());
         }
+    }
 
-        //This method will then be called every time a ChatMessage is sent.
-        private bool ChatMessage(ClientInfo clientInfo, EChatType _type, int _senderId, string message, string mainName, bool _localizeMain, List<int> _recipientEntityIds)
+    [HarmonyPatch(typeof(EntityPlayerLocal))]
+    [HarmonyPatch("Init")]
+    public class EntityPlayerLocal_Init
+    {
+        static void Postfix(EntityPlayerLocal __instance, int _entityClass)
         {
-            //We make sure there is an actual message and a client, and also ignore the message if it's from the server.
-            if (!string.IsNullOrEmpty(message) && clientInfo != null && mainName != ServerChatName)
+            if (VorpX.VPX_RESULT.VPX_RES_OK != VorpX.vpxInit())
             {
-                //We check to see if the message starts with a /
-                if (message.StartsWith("/"))
-                {
-                    //we then remove that / to get the rest of the message.
-                    message = message.Replace("/", "");
-
-                    if (message == "hello")
-                    {
-                        ChatHook.ChatMessage(clientInfo, $"Hello {clientInfo.playerName}", -1, Config.Server_Response_Name, EChatType.Global, null);
-                        //We return false to prevent any other listeners from processing this message.
-                        return false;
-                    }
-                }
+                Log.Out($"VorpX fatal error when trying to initialize");
+                return;
             }
-            //Returning true allows other listeners to process this message.
-            return true;
+
+            var instanceCamera = __instance.cameraTransform.GetComponent<Camera>();
+
+            if (instanceCamera)
+            {
+                API.VRHeadsetFOV = VorpX.vpxGetHeadsetFOV();
+            }
+
+            instanceCamera.fieldOfView = API.VRHeadsetFOV;
+        }
+    }
+
+    [HarmonyPatch(typeof(EntityPlayerLocal))]
+    [HarmonyPatch("LateUpdate")]
+    public class EntityPlayerLocal_LateUpdate
+    {
+        static void Postfix(EntityPlayerLocal __instance)
+        {
+            Transform cameraTransform = __instance.cameraTransform;
+            var instanceCamera = cameraTransform.GetComponent<Camera>();
+
+            if (instanceCamera)
+            {
+                API.VRHeadsetFOV = VorpX.vpxGetHeadsetFOV();
+            }
+            instanceCamera.fieldOfView = API.VRHeadsetFOV;
+
+            var headsetRotation4f = VorpX.vpxGetHeadsetRotationQuaternion();
+            var headsetPosition3f = VorpX.vpxGetHeadsetPosition();
+
+            Quaternion headsetRotation = new Quaternion(-headsetRotation4f.x, -headsetRotation4f.y, headsetRotation4f.z, headsetRotation4f.w);
+            Vector3 headsetPosition = new Vector3(headsetPosition3f.x, headsetPosition3f.y, headsetPosition3f.z);
+
+
+            var controllerState = VorpX.vpxGetControllerState(0);
+
+            __instance.movementInput.rotation = new Vector3(headsetRotation.x, headsetRotation.y, headsetRotation.z);
+            __instance.movementInput.moveForward = controllerState.StickY;
+            __instance.movementInput.moveStrafe = controllerState.StickX;
+
+            __instance.playerCamera.transform.position = __instance.cameraTransform.position + headsetPosition;
         }
     }
 }
---
+++ VorpX.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public static class VorpX
+{
+    const string dllName = @"C:\Users\SKIKK\Documents\vorpAPI.dll";
+
+    // Import the C API functions as external functions in C#
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern VPX_RESULT vpxInit();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern VPX_RESULT vpxFree();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern VPX_BOOL vpxIsActive();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern float vpxGetHeadsetFOV();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern float vpxVertToHorFOV(float fov_deg, float aspect);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern vpxfloat3 vpxGetHeadsetRotationEuler();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern vpxfloat4 vpxGetHeadsetRotationQuaternion();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern vpxfloat3 vpxGetHeadsetPosition();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern VPX_CONTROLLER_STATE vpxGetControllerState(uint controllerNum);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern vpxfloat3 vpxGetControllerRotationEuler(uint cotrollerNum);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern vpxfloat4 vpxGetControllerRotationQuaternion(uint controllerNum);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern vpxfloat3 vpxGetControllerPosition(uint controllerNum);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void vpxForceControllerRendering(VPX_BOOL val);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern vpxfloat3 vpxYawCorrection(vpxfloat3 position, float yawInDegrees);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void vpxRequestEdgePeek(VPX_BOOL val);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern VPX_BOOL vpxGetEdgePeekRequested();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern VPX_BOOL vpxGetEdgePeekActual();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern vpxfloat3 vpxGetEyePosition(uint eye, float ipd);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern float vpxGetG3DStrength();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int vpxGetCurrentVorpFrame();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern VPX_BOOL vpxIsVorpDoing3D();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void vpxSetGameCamRotationDeltaEuler(vpxfloat3 rotation);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void vpxSetStereoReduction(float reduction);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void vpxInitStereo();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void vpxSetGameStereoRenderMode(VPX_GAME_STEREO_MODE stereoMode);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void vpxSetAlternateFrame3DEye(uint eye);
+
+    public static float vpxDegToRad(float a)
+    {
+        return a * 0.0174532925f;
+    }
+
+    public static float vpxRadToDeg(float a)
+    {
+       return a * 57.295795131f;
+    }
+
+    // Define C# versions of the C structs
+    [StructLayout(LayoutKind.Sequential)]
+    public struct vpxint2
+    {
+        public int x;
+        public int y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct vpxint3
+    {
+        public int x;
+        public int y;
+        public int z;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct vpxint4
+    {
+        public int x;
+        public int y;
+        public int z;
+        public int w;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct vpxfloat2
+    {
+        public float x;
+        public float y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct vpxfloat3
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct vpxfloat4
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct vpxmtx4x4
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        public float[] m;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct VPX_CONTROLLER_STATE
+    {
+        public bool IsActive;      // VPX_TRUE if active, otherwise VPX_FALSE
+        public float StickX;       // Thumbstick/pad x-axis [-1|1]
+        public float StickY;       // Thumbstick/pad y-axis [-1|1]
+        public float Trigger;      // Trigger axis [0|1]
+        public float Grip;         // Grip axis [0|1], on controllers with a grip button (e.g. Vive wands) either 0.0 or 1.0
+        public float Extra0;       // Extra axis (for future use)
+        public float Extra1;       // Extra axis (for future use)
+        public float Extra2;       // Extra axis (for future use)
+        public float Extra3;       // Extra axis (for future use)
+        public float Finger0;      // Finger axis: thumb (for future use)
+        public float Finger1;      // Finger axis: index (for future use)
+        public float Finger2;      // Finger axis: middle (for future use)
+        public float Finger3;      // Finger axis: ring (for future use)
+        public float Finger4;      // Finger axis: pinky (for future use)
+        public uint ButtonsPressed; // Check with a flag, e.g.: if (ButtonsPressed & VPX_CONTROLLER_BUTTON_0)
+        public uint ButtonsTouched; // Check with a flag, e.g.: if (ButtonsTouched & VPX_CONTROLLER_BUTTON_0)
+    }
+
+    public enum VPX_GAME_STEREO_MODE
+    {
+        VPX_GAME_STEREO_MODE_NONE = 0,
+        VPX_GAME_STEREO_MODE_SINGLE_FRAME = 1,
+        VPX_GAME_STEREO_MODE_ALTERNATE_FRAME = 2
+    }
+
+    public enum VPX_BOOL
+    {
+        VPX_FALSE = 0,
+        VPX_TRUE = 1,
+    }
+
+    public enum VPX_RESULT
+    {
+        VPX_RES_OK = 0,
+        VPX_RES_FAIL = 1,
+        VPX_RES_INVALID_ARGUMENT = 101,
+        VPX_RES_NULL_POINTER = 102,
+        VPX_RES_NOT_INITIALIZED = 103,
+        VPX_RES_FUNCTION_UNAVAILABLE = 104
+    }
+
+    public enum VPX_CONTROLLER_BUTTON
+    {
+        VPX_CONTROLLER_BUTTON_0 = 0x001,
+        VPX_CONTROLLER_BUTTON_1 = 0x002,
+        VPX_CONTROLLER_BUTTON_MENU = 0x004,
+        VPX_CONTROLLER_BUTTON_STICK_PAD_0 = 0x008,
+        VPX_CONTROLLER_BUTTON_TRIGGER = 0x010,
+        VPX_CONTROLLER_BUTTON_GRIP = 0x020
+    }
+}
+
